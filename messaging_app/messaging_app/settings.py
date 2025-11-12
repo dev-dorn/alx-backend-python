a40f8f4102e26d1ca21301a99ff25fb48fa7ba0e
dev-dorn <berrymundia@gmail.com>
2025-11-12 21:30:15 +0300

    done AND DUSTED

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -55,12 +55,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('MYSQL_HOST', 'db'),
+        'PORT': os.getenv('MYSQL_PORT', '3306'),
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- messaging_app/venv/bin/activate.csh
+++ messaging_app/venv/bin/activate.csh
@@ -9,7 +9,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV '/home/don/alx projects/alx-backend-python/messaging_app/venv'
+setenv VIRTUAL_ENV /home/godzilla/alx/alx-backend-python/messaging_app/venv
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/"bin":$PATH"
